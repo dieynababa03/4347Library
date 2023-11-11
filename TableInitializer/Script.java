1bbb12a9892aea16c8a62efd256bc8f5963b9659
Dieynaba Ba <dieynaba.ba6675@gmail.com>
2023-11-11 14:18:35 -0600

    Chnges to Script.java for duplicate authors

--- TableInitializer/Script.java
+++ TableInitializer/Script.java
@@ -17,11 +17,10 @@ public class Script {
 
       // Reading book information from CSV file
       try (BufferedReader br = new BufferedReader(
-          new FileReader("/Users/dieynababa/4347/4347Library/TableInitializer/books.csv"))) {
+          new FileReader("/Users/dieynababa/4347Library/4347Library/TableInitializer/books_2.csv"))) {
 
         String header = br.readLine();
         String line;
-        line = br.readLine(); // Read the first line
 
         // Reading line by line
         while ((line = br.readLine()) != null) {
@@ -37,7 +36,7 @@ public class Script {
           // Inserting into Authors table
           Statement insertionIntoAuthors = myConn.createStatement();
           Statement insertionIntoBookAuthors = myConn.createStatement();
-
+          Statement authorIdRetrieval = myConn.createStatement();
           // Splitting Authors line for when there's more than one author
           String names[] = values[3].split(",");
 
@@ -45,19 +44,42 @@ public class Script {
             // Iterating when a book has multiple authors as well as inserting into the book_authors table
             for (int i = 0; i < names.length; i++) {
               names[i] = names[i].replace("\"", "");
-              String insertionIntoAuthorsQuery = "INSERT INTO AUTHORS (Name) VALUES (" + '"' + names[i] + '"' + ");";
+              String insertionIntoAuthorsQuery = "INSERT INTO AUTHORS (Name) SELECT (" + '"' + names[i].replace("\"", "") + '"'
+                  + ") FROM dual  WHERE NOT EXISTS (SELECT Name FROM AUTHORS WHERE Name=" + '"' + names[i].replace("\"", "") + '"' + ");";
               insertionIntoAuthors.executeUpdate(insertionIntoAuthorsQuery);
-              // LAST_INSERT_ID returns the id for the author just added
-              String insertionIntoBookAuthorsQuery = "INSERT INTO BOOK_AUTHORS(Author_id, Isbn) VALUES("
-                  + "LAST_INSERT_ID()" + ", " + '"' + values[0] + '"' + ");";
+
+              // Get Author_ID
+              String getAuthorIDQuery = "SELECT Author_id FROM AUTHORS WHERE Name=" + '"' + names[i].replace("\"", "") + '"';
+              ResultSet res = authorIdRetrieval.executeQuery(getAuthorIDQuery);
+              int authorID = 0;
+              if (res.next()) {
+                authorID = res.getInt("Author_id");
+              }
+
+              //Only insert into Book_Authors if the author id does not exist
+              String insertionIntoBookAuthorsQuery = "INSERT INTO BOOK_AUTHORS(Author_id, Isbn) SELECT " + authorID + ", " + '"' + values[0] + '"' + " FROM dual WHERE NOT EXISTS (" + "SELECT Author_id FROM BOOK_AUTHORS WHERE Author_id=" + authorID + " AND Isbn=" + '"' + values[0] + '"' + ");";
+
+
+                  
               insertionIntoBookAuthors.executeUpdate(insertionIntoBookAuthorsQuery);
             }
           } else {
             // Same as above but in the case where there is only 1 author
-            String insertionIntoAuthorsQuery = "INSERT INTO AUTHORS (Name) VALUES (" + '"' + names[0].replace("\"", "") + '"' + ");";
+            String insertionIntoAuthorsQuery = "INSERT INTO AUTHORS (Name) SELECT (" + '"' + names[0].replace("\"", "") + '"'
+                + ") FROM dual  WHERE NOT EXISTS (SELECT Name FROM AUTHORS WHERE Name=" + '"' + names[0].replace("\"", "") + '"' + ");";
             insertionIntoAuthors.executeUpdate(insertionIntoAuthorsQuery);
+
+            // Get Author_ID
+            String getAuthorIDQuery = "SELECT Author_id FROM AUTHORS WHERE Name=" + '"' + names[0].replace("\"", "") + '"';
+            ResultSet res = authorIdRetrieval.executeQuery(getAuthorIDQuery);
+            int authorID = 0;
+            if (res.next()) {
+              authorID = res.getInt("Author_id");
+            }
+
+            //Insert into Book_Authors
             String insertionIntoBookAuthorsQuery = "INSERT INTO BOOK_AUTHORS(Author_id, Isbn) VALUES("
-                + "LAST_INSERT_ID()" + ", " + '"' + values[0] + '"' + ");";
+                + authorID + ", " + '"' + values[0] + '"' + ");";
             insertionIntoBookAuthors.executeUpdate(insertionIntoBookAuthorsQuery);
           }
 
