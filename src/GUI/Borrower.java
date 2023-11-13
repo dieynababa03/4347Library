ed44ee492c9d5870b0fe703728fd101037939b17
jeremyrdavidson <78677500+jeremyrdavidson@users.noreply.github.com>
2023-11-12 23:45:24 -0600

    resize Borrower.java

--- src/GUI/Borrower.java
+++ src/GUI/Borrower.java
@@ -177,7 +177,7 @@ public class Borrower extends javax.swing.JFrame {
                                 .addGap(34, 34, 34)
                                 .addComponent(jLabel8)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel2)
