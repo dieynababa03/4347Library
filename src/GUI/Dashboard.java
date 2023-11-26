f21ccc6573bc0305d29771fc3b58a054ded43dc6
jeremyrdavidson <78677500+jeremyrdavidson@users.noreply.github.com>
2023-11-26 14:03:27 -0600

    fines toggle Dashboard.java

--- src/GUI/Dashboard.java
+++ src/GUI/Dashboard.java
@@ -168,7 +168,9 @@ public class Dashboard extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        Fines fm = new Fines();
+        fm.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
